83b363ce51dd5261e347dc4a63f2a963120cfae3
Levva01 <vellanifederico13@gmail.com>
2022-09-26 16:32:20 +0200

    Milestone 3

--- Program.cs
+++ Program.cs
@@ -26,4 +26,56 @@ do
 Console.Write($"Evento: {e.Titolo}:" +
     $"Data: {e.Date.ToString()}" +
     $"Capienza: {e.Capienza}" +
-    $"Posti Prenotati: {e.PostiPrenotati}");
+    $"Posti Prenotati: {e.PostiPrenotati}"
+);
+
+public class ProgrammaEventi
+{
+    public ProgrammaEventi(string titolo)
+    {
+        Titolo = titolo;
+        Eventi = new List<Evento>();
+    }
+
+    public string Titolo { get; set; }
+    public List<Evento> Eventi { get; set; }
+    
+    public void aggiungiEvento(Evento e)
+    {
+        this.Eventi.Add(e);
+    }
+
+    public List<Evento> getEventsFromDate(List<Evento> e)
+    {
+        string dateString = Console.ReadLine();
+        DateOnly date = DateOnly.Parse(dateString);
+        
+        List<Evento> eventsDate = new List<Evento>();
+        foreach (Evento evento in e)
+        {
+            if(evento.Date == date)
+            {
+                eventsDate.Add(evento);
+            }
+        }
+        return eventsDate;
+    }
+
+    public static void StampaEventi(List<Evento> e)
+    {
+        foreach(Evento i in e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+    }
+
+    public void CancellaLista()
+    {
+        Eventi.Clear();
+    }
+
+    public int ContaEventi()
+    {
+        return Eventi.Count;
+    }
+}
