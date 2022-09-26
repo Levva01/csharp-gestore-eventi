606f8c2833ace321038e238efdc18e3a334ae975
Levva01 <vellanifederico13@gmail.com>
2022-09-26 11:02:21 +0200

    Creazione attributi con get e set

--- Program.cs
+++ Program.cs
@@ -1,2 +1,53 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+public class Evento
+{
+    public string Titolo {
+        get => Titolo;
+        set
+        {
+            if (!string.IsNullOrEmpty(value))
+                Titolo = value;
+            else Console.WriteLine("String vuota");
+        }
+    }
+
+    public DateOnly Date { 
+        get => Date;
+        set
+        {
+            if (value >= DateOnly.FromDateTime(DateTime.Now))
+                Date = value;
+            else Console.WriteLine("Data passata");
+
+        }
+    }
+
+    public int CapienzaMassima { 
+        get => CapienzaMassima;
+        private set
+        {
+            if (value >= 0)
+                CapienzaMassima = value;
+            else Console.WriteLine("La capienza massima non può essere negativa");
+        }
+    }
+
+    public int PostiPrenotati { 
+        get => PostiPrenotati;
+        private set 
+        {
+            PostiPrenotati = value;    
+        }
+    }
+
+    public Evento(string titolo, DateOnly date, int capienzaMassima)
+    {
+        Titolo = titolo;
+        Date = date;
+        CapienzaMassima = capienzaMassima;
+        PostiPrenotati = 0;
+    }
+}
